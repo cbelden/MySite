b5865eba153992ed5c36e617873256ca9b5ee144
Calvin Belden <cbelden@nd.edu>
2013-07-19 08:50:46 -0500

    Added a lot of formatting, fixed some content, configured navbar, filled in the about page

--- mysite/page_not_found_message.py
+++ mysite/page_not_found_message.py
@@ -9,18 +9,19 @@ def get_404_message():
     """)
 
     candidates.append("""
-    Oops! My bad yo.  I haven't created this page yet.<br>
-    Navigate 'back' on your browser or click <a href="/">here</a>.
+    Oops! My bad yo.  I haven't created this page yet.  Navigate 'back' on your browser
+    or click on <a href="/">any of these words</a> to get to the main page.
     """)
 
     candidates.append("""
     <em>
-        IF YOU'VE MADE IT TO THIS PAGE, SOMETHING TERRIBLE HAS HAPPENED, AND I'VE<br>
-        BEEN FORCED TO FLEE THE COMFORT OF THE COVETED CORNER TABLE AT BARNES & NOBLE.
+        IF YOU'VE MADE IT TO THIS PAGE, SOMETHING TERRIBLE HAS HAPPENED, AND I'VE
+        FLED THE COVETED CORNER COFFEE TABLE AT BARNES AND NOBLE.
     </em>
     <br>
     <br>
-    My fishing <a href="/">home</a> in Cinque Terre may be my last safe haven; meet me there. Put a pot of coffee on, will ya?
+    My fishing <a href="/">home</a> in Cinque Terre may be my last safe haven; meet me
+    there.  Put a pot of coffee on, will ya?
     """)
 
     index = random.randint(0, len(candidates) - 1)
--- mysite/views.py
+++ mysite/views.py
@@ -4,17 +4,24 @@ from mysite import app
 from flask import render_template
 
 
+# -------  primary page handlers -------
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+# @app.route('/projects')
+# def projects():
+#     return render_template('projects.html')
+
+
+# ---------- error handlers -----------
 @app.errorhandler(404)
 def page_not_found(e):
     message = pnfm.get_404_message()
     return render_template('404.html', error_message=message), 404
-
-
-@app.route('/practice')
-def practice():
-    return render_template('practice.html')
